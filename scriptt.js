59dd2fef3fcec2c8be0deaf2e89d985972bccb26
Li-hype <livia.m.rodrigues@aluno.senai.br>
2025-03-28 11:21:25 -0300

    adicionando atividade

--- scriptt.js
+++ scriptt.js
@@ -1,15 +1,30 @@
 let titulo = document.getElementById("titulo")
 console.log(titulo);
 
+let esporte = document.getElementsByClassName("esp")
+console.log(esporte[0].textContent);
 
-let esp = document.getElementsByClassName("esp")
-console.log(esp[0]);
+let esporte1 = document.getElementsByClassName("esp")
+console.log(esporte[1].textContent);
 
-let lista = document.getElementsByClassName("ul")
-console.log(lista[0]);
+let esporte2 = document.getElementsByClassName("esp")
+console.log(esporte[2].textContent);
+
+let esporte3 = document.getElementsByClassName("esp")
+console.log(esporte[3].textContent);
 
 let tituloQS = document.querySelector("#titulo");
 console.log("Exibindo com Query Selector" + tituloQS.textContent);
 
 let esportesQS = document.querySelector(".esp");
-console.log("Exibindo com Query Selector" + esportesQS.textContent);
+console.log("Exibindo com Query Selector" + esportesQS[0]);
+
+
+let esportesQS1 = document.querySelector(".esp");
+console.log("Exibindo com Query Selector" + esportesQS[1]);
+
+let esportesQS2 = document.querySelector(".esp");
+console.log("Exibindo com Query Selector" + esportesQS[2]);
+
+let esportesQS3 = document.querySelector(".esp");
+console.log("Exibindo com Query Selector" + esportesQS[3]);
